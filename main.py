c51a67a176f4af24b9a496842d55594a4cc1fd2f
Henrik L <helagro@outlook.com>
2023-09-16 12:22:11 +0200

    unspecified

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os 
-import time
-import math
 import sys
-import re
-
 from settings.__main__ import Settings
+from shouldArchive import ShouldArchive
 
 
 argument = "" if len(sys.argv) < 2 else sys.argv[1]
+
 settings = Settings()
+shouldArchive = ShouldArchive(
+    settings["excluded"], 
+    settings["defaultRules"]
+)
 
 
 def main():
@@ -27,54 +29,13 @@ def archiveOldFiles(folderItem):
     for itemName in os.listdir(folderItem["path"]):
 
         itemPath = folderItem["path"] + os.sep + itemName
-        if isItemExcepted(itemPath): continue
+        rules = folderItem["rules"]
 
-        rule = getFirstMatchingRule(folderItem["rules"], itemPath)
-        if rule is None: continue
-
-        itemAge = getItemAge(itemPath)
-        if itemAge >= rule["archiveAfterDays"]:
-            if argument == "do":
-                archiveItem(itemPath)
-            else:
-                print("Would have archived:", itemPath)
-
-
-def getItemAge(path):
-    timeStamps = [
-        os.path.getmtime(path), #modified
-        os.path.getatime(path), #accessed
-        os.path.getctime(path) #changed
-    ]
-    
-    ageInSeconds = time.time() - max(timeStamps)
-    ageInDays = ageInSeconds / 3600 / 24
-    return math.floor(ageInDays)
-
-
-def isItemExcepted(itemPath):
-    for exception in settings["exclude"]:
-        if re.match(exception, itemPath):
-            return True
-
-    return False
-
-
-def getFirstMatchingRule(rulesForFolder, itemPath):
-    rules = rulesForFolder + settings["defaultRules"]
-    for rule in rules:
-        ruleMatches = doesRuleMatch(rule["pattern"], itemPath)
-        if ruleMatches:
-            return rule
-
-
-def doesRuleMatch(rulePattern, itemPath):
-    if rulePattern == "<folder>":
-        return os.path.isdir(itemPath)
-    if re.match(rulePattern, itemPath):
-        return True
-    return False
-
+        doArchive = shouldArchive.eval(itemPath, rules)
+        if doArchive:
+            archiveItem(itemPath)
+        else:
+            print("Would have archived:", itemPath)
 
 
 
---
+++ shouldArchive.py
@@ -0,0 +1,57 @@
+import time
+import math
+import re
+import os
+
+class ShouldArchive:
+
+    def __init__(self, excluded, defaultRules):
+        self.excluded = excluded
+        self.defaultRules = defaultRules
+
+
+
+    def eval(self, path, rules):
+        if self.__isItemExcepted(path): return False
+
+        rule = self.__getFirstMatchingRule(rules, path)
+        if rule is None: return False
+
+        age = self.__getItemAge(path)
+        return age >= rule["archiveAfterDays"]
+
+
+    def __isItemExcepted(self, itemPath):
+        for exception in self.excluded:
+            if re.match(exception, itemPath):
+                return True
+
+        return False
+
+
+    def __getFirstMatchingRule(self, rulesForFolder, itemPath):
+        rules = rulesForFolder + self.defaultRules
+        for rule in rules:
+            ruleMatches = self.__doesRuleMatch(rule["pattern"], itemPath)
+            if ruleMatches:
+                return rule
+
+
+    def __doesRuleMatch(self, rulePattern, itemPath):
+        if rulePattern == "<folder>":
+            return os.path.isdir(itemPath)
+        if re.match(rulePattern, itemPath):
+            return True
+        return False
+    
+
+    def __getItemAge(self, path):
+        timeStamps = [
+            os.path.getmtime(path), #modified
+            os.path.getatime(path), #accessed
+            os.path.getctime(path) #changed
+        ]
+        
+        ageInSeconds = time.time() - max(timeStamps)
+        ageInDays = ageInSeconds / 3600 / 24
+        return math.floor(ageInDays)
